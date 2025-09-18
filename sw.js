9b7ff2dbefe83280efdbfbe06dbfd12710f741e1
johnnypot99-cyber <johnnypot99@gmail.com>
2025-09-18 17:53:41 -0400

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,3 @@
+
+self.addEventListener('install', e => e.waitUntil(caches.open('btwp-cal-v1').then(c=>c.addAll(['./']))));
+self.addEventListener('fetch', e => e.respondWith(caches.match(e.request).then(r => r || fetch(e.request))));
